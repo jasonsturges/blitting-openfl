4168fdd92f810e3b4ec36f3fda675e8b2b945211
Jason Sturges <jason@jasonsturges.com>
2014-11-30 01:19:30 -0600

    Initial import openfl create template.

---
+++ Source/Main.hx
@@ -0,0 +1,13 @@
+package;
+
+import com.blitting.display.Viewport;
+import flash.display.StageAlign;
+import flash.display.StageScaleMode;
+
+class Main extends Viewport {
+
+    public function new() {
+        super();
+    }
+
+}
