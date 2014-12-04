ef94ae85bf8b3d7bea7572684be85694f6961d09
Jason Sturges <jason@jasonsturges.com>
2014-12-04 01:56:15 -0600

    OpenFL Haxe port of Geometry.

--- Source/com/blitting/math/Geometry.hx
+++ Source/com/blitting/math/Geometry.hx
@@ -4,7 +4,7 @@ import openfl.geom.Rectangle;
 
 class Geometry {
 
-/**
+    /**
      * Convert radians to degrees.
      */
     public static function deg2rad(angle:Float):Float {
@@ -13,11 +13,10 @@ class Geometry {
         return angle * Math.PI / 180;
     }
 
-/**
+    /**
      * Distance between two points within the same
      * coordinate system.
      */
-
     public static function distance(x1:Float, y1:Float, x2:Float, y2:Float):Float {
         var dx:Float = x1 - x2;
         var dy:Float = y1 - y2;
@@ -25,10 +24,9 @@ class Geometry {
         return Math.sqrt(dx * dx + dy * dy);
     }
 
-/**
+    /**
      * Calculate angle of a point from a center point.
      */
-
     public static function polarAngle(point:Point, center:Point = null):Float {
         if (center != null)
             center = new Point(0, 0);
@@ -41,10 +39,9 @@ class Geometry {
         return degrees;
     }
 
-/**
+    /**
      * Returns the x, y coordinate point on a circle for a given angle in degrees.
      */
-
     public static function pointOnCircle(degrees:Float, radius:Float):Point {
         var p:Point = new Point();
 
@@ -54,10 +51,9 @@ class Geometry {
         return p;
     }
 
-/**
+    /**
      * Cartesian point from circle center, angle, and radius.
      */
-
     public static function pointOnCircleFromPoint(center:Point, angle:Float, radius:Float):Point {
         var p:Point = new Point();
 
@@ -67,19 +63,17 @@ class Geometry {
         return p;
     }
 
-/**
+    /**
      * Returns the points on a bezier curve for a given time (0 - 1).
      */
-
     public static function pointOnCurve(p1x:Float, p1y:Float, cx:Float, cy:Float, p2x:Float, p2y:Float, t:Float):Point {
         return (new Point(p1x + t * (2 * (1 - t) * (cx - p1x) + t * (p2x - p1x)),
         p1y + t * (2 * (1 - t) * (cy - p1y) + t * (p2y - p1y))));
     }
 
-/**
+    /**
      * Convert degrees to radians.
      */
-
     public static function rad2deg(angle:Float):Float {
         angle = !Math.isNaN(angle) ? (angle) : (0);
 
@@ -89,7 +83,6 @@ class Geometry {
     /**
      * Return a random degree.
      */
-
     public static function randomAngle():Float {
         return (Math.random() * 360);
     }
@@ -106,7 +99,6 @@ class Geometry {
      * @param bounds    Rectangle boundary to be tested against.
      * @return          True if point is within bounds.
      */
-
     public static function withinBounds(x:Float, y:Float, bounds:Rectangle):Bool {
         return ((x > bounds.x) &&
         (x < bounds.x + bounds.width) &&
