568aee0ceb260fa01011d59203e445f07f738777
Jason Sturges <jason@jasonsturges.com>
2014-12-04 16:35:16 -0600

    OpenFL Haxe port of Statistics.

---
+++ Source/com/blitting/math/Statistics.hx
@@ -0,0 +1,8 @@
+package com.blitting.math;
+class Statistics {
+
+    public static function probability(occurred:Float, outcomes:Float):Bool {
+        return Math.random() * outcomes < occurred;
+    }
+
+}
