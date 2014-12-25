8acb2f476a706d58fdebebca2477b11bad707e7e
Jason Sturges <jason@jasonsturges.com>
2014-12-24 18:21:41 -0600

    Import bitmap renderer.

--- blitting/display/BitmapRenderer.hx
+++ blitting/display/BitmapRenderer.hx
@@ -9,7 +9,41 @@
 */
 package blitting.display;
 
-class BitmapRenderer {
+import openfl.display.BitmapData;
+import blitting.display.AbstractRenderer;
+import blitting.display.IBitmapRenderer;
+
+class BitmapRenderer extends AbstractRenderer implements IBitmapRenderer {
+
+    //------------------------------
+    //  model
+    //------------------------------
+
+    private var bitmapData:BitmapData;
+
+
+    //------------------------------
+    //  lifecycle
+    //------------------------------
+
     public function new() {
+        super();
     }
+
+    override public function initialize():Void {
+        super.initialize();
+
+        bitmapData = null;
+    }
+
+    public function render(bitmapData:BitmapData):Void {
+        this.bitmapData = bitmapData;
+    }
+
+    override public function dispose():Void {
+        super.dispose();
+
+        bitmapData = null;
+    }
+
 }
