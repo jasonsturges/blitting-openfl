95c693228962dbe058b719936fd56279ea8d9757
Jason Sturges <jason@jasonsturges.com>
2014-12-24 18:21:20 -0600

    Import graphics renderer.

--- blitting/display/GraphicsRenderer.hx
+++ blitting/display/GraphicsRenderer.hx
@@ -9,7 +9,47 @@
 */
 package blitting.display;
 
-class GraphicsRenderer {
+import openfl.display.DisplayObject;
+import openfl.display.Graphics;
+import blitting.display.AbstractRenderer;
+import blitting.display.IGraphicsRenderer;
+
+class GraphicsRenderer extends AbstractRenderer implements IGraphicsRenderer {
+
+    //------------------------------
+    //  model
+    //------------------------------
+
+    private var displayObject:DisplayObject;
+
+    private var graphics:Graphics;
+
+
+    //------------------------------
+    //  lifecycle
+    //------------------------------
+
     public function new() {
+        super();
     }
+
+    override public function initialize():Void {
+        super.initialize();
+
+        displayObject = null;
+        graphics = null;
+    }
+
+    public function render(graphics:Graphics, ?displayObject:DisplayObject):Void {
+        this.displayObject = displayObject;
+        this.graphics = graphics;
+    }
+
+    override public function dispose():Void {
+        super.dispose();
+
+        displayObject = null;
+        graphics = null;
+    }
+
 }
