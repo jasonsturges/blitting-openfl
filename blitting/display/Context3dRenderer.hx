95998d6ca893df414df3a64860c8173b36bdc37f
Jason Sturges <jason@jasonsturges.com>
2014-12-24 18:21:52 -0600

    Import context3d renderer.

--- blitting/display/Context3dRenderer.hx
+++ blitting/display/Context3dRenderer.hx
@@ -9,7 +9,42 @@
 */
 package blitting.display;
 
-class Context3dRenderer {
-    public function new() {
+import openfl.display3D.Context3D;
+import openfl.display3D.IndexBuffer3D;
+import openfl.display3D.Program3D;
+import openfl.display3D.VertexBuffer3D;
+import blitting.display.AbstractRenderer;
+
+class Context3dRenderer extends AbstractRenderer {
+
+    //------------------------------
+    //  model
+    //------------------------------
+
+    private var program3d:Program3D;
+
+    private var vertexBuffer3d:VertexBuffer3D;
+
+    private var indexBuffer3d:IndexBuffer3D;
+
+
+    //------------------------------
+    //  lifecycle
+    //------------------------------
+
+    public function Context3dRenderer() {
+        super();
     }
+
+    public function render(context3d:Context3D):Void {
+    }
+
+    override public function dispose():Void {
+        super.dispose();
+
+        program3d = null;
+        vertexBuffer3d = null;
+        indexBuffer3d = null;
+    }
+
 }
