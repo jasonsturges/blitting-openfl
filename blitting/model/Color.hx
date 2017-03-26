121f637556c91ef0236b8259c0c7e59b4457bb90
Jason Sturges <jason@jasonsturges.com>
2017-03-26 16:25:16 -0500

    Code format

--- blitting/model/Color.hx
+++ blitting/model/Color.hx
@@ -15,15 +15,14 @@ import blitting.lifecycle.IInitializable;
 /**
  * Color structure.
  */
-class Color implements IInitializable
-{
-    public var rgb(get, never) : Int;
-    public var argb(get, never) : Int;
-    public var rgba(get, never) : Int;
-    public var red(get, never) : Int;
-    public var green(get, never) : Int;
-    public var blue(get, never) : Int;
-    public var alpha(get, never) : Int;
+class Color implements IInitializable {
+    public var rgb(get, never):Int;
+    public var argb(get, never):Int;
+    public var rgba(get, never):Int;
+    public var red(get, never):Int;
+    public var green(get, never):Int;
+    public var blue(get, never):Int;
+    public var alpha(get, never):Int;
 
 
     //------------------------------
@@ -38,14 +37,14 @@ class Color implements IInitializable
      * 8-bit green  (0x0 - 0xff)
      * 8-bit blue   (0x0 - 0xff)
      */
-    private var color : Int;
+    private var color:Int;
 
     /**
      * Get 24-bit unsigned int of 8-bit red, green, and blue
      *
      * @return 24-bit unsigned int of 8-bit red, green, and blue.
      */
-    private function get_rgb() : Int{
+    private function get_rgb():Int {
         return 0xffffff & color;
     }
 
@@ -54,7 +53,7 @@ class Color implements IInitializable
      *
      * @return 32-bit unsigned int of 8-bit alpha, red, green, blue.
      */
-    private function get_argb() : Int{
+    private function get_argb():Int {
         return color;
     }
 
@@ -63,7 +62,7 @@ class Color implements IInitializable
      *
      * @return 32-bit unsigned int of 8-bit red, green, blue, alpha.
      */
-    private function get_rgba() : Int{
+    private function get_rgba():Int {
         return color << 8 | color >>> 24;
     }
 
@@ -72,7 +71,7 @@ class Color implements IInitializable
      *
      * @return Unsigned int of 8-bit red.
      */
-    private function get_red() : Int{
+    private function get_red():Int {
         return color >>> 16 & 0xff;
     }
 
@@ -80,7 +79,7 @@ class Color implements IInitializable
      * Get unsigned int of 8-bit green.
      * @return Unsigned int of 8-bit green.
      */
-    private function get_green() : Int{
+    private function get_green():Int {
         return color >>> 8 & 0xff;
     }
 
@@ -89,7 +88,7 @@ class Color implements IInitializable
      *
      * @return Unsigned int of 8-bit blue.
      */
-    private function get_blue() : Int{
+    private function get_blue():Int {
         return color & 0xff;
     }
 
@@ -98,7 +97,7 @@ class Color implements IInitializable
      *
      * @return Unsigned int of 8-bit alpha.
      */
-    private function get_alpha() : Int{
+    private function get_alpha():Int {
         return color >>> 24 & 0xff;
     }
 
@@ -116,8 +115,7 @@ class Color implements IInitializable
      * @param alpha  8-bit alpha.
      *
      */
-    public function new(red : Int = 0x0, green : Int = 0x0, blue : Int = 0x0, alpha : Int = 0xff)
-    {
+    public function new(red:Int = 0x0, green:Int = 0x0, blue:Int = 0x0, alpha:Int = 0xff) {
         color = alpha << 24 | red << 16 | green << 8 | blue;
     }
 
@@ -125,7 +123,7 @@ class Color implements IInitializable
      * IInitialize initialization.
      *
      */
-    public function initialize() : Void{
+    public function initialize():Void {
         color = 0x0;
     }
 
@@ -135,11 +133,11 @@ class Color implements IInitializable
      * @param color  32-bit ARGB color as unsigned integer.
      * @return       Color instance of 8-bit alpha, red, green, blue.
      */
-    public static function fromARGB(color : Int) : Color{
-        return new Color(color >>> 16 & 0xff,   // red  
-        color >>> 8 & 0xff,   // green  
-        color & 0xff,   // blue  
-        color >>>24  // alpha  , 
+    public static function fromARGB(color:Int):Color {
+        return new Color(color >>> 16 & 0xff, // red
+        color >>> 8 & 0xff, // green
+        color & 0xff, // blue
+        color >>> 24 // alpha  ,
         );
     }
 
@@ -154,11 +152,11 @@ class Color implements IInitializable
      * @param color  32-bit ARGB color as unsigned integer.
      * @return       Color instance of 8-bit red, green, blue, alpha.
      */
-    public static function fromRGBA(color : Int) : Color{
-        return new Color(color >>> 24,   // red  
-        color >>> 16 & 0xff,   // green  
-        color >>> 8 & 0xf,   // blue  
-        color &0xff  // alpha  , 
+    public static function fromRGBA(color:Int):Color {
+        return new Color(color >>> 24, // red
+        color >>> 16 & 0xff, // green
+        color >>> 8 & 0xf, // blue
+        color & 0xff // alpha
         );
     }
 
@@ -168,10 +166,10 @@ class Color implements IInitializable
      * @param color  24-bit RGB color as unsigned integer.
      * @return       Color instance of 8-bit red, green, blue.
      */
-    public static function fromRGB(color : Int) : Color{
-        return new Color(color >> 16 & 0xff,   // red  
-        color >> 8 & 0xff,   // green  
-        color &0xff  // blue  , 
+    public static function fromRGB(color:Int):Color {
+        return new Color(color >> 16 & 0xff, // red
+        color >> 8 & 0xff, // green
+        color & 0xff // blue
         );
     }
 
@@ -196,7 +194,7 @@ class Color implements IInitializable
      * @param blue   8-bit Blue.
      * @return       32-bit ARGB unsigned integer.
      */
-    public static function getARGB(alpha : Int, red : Int, green : Int, blue : Int) : Int{
+    public static function getARGB(alpha:Int, red:Int, green:Int, blue:Int):Int {
         return alpha << 24 | red << 16 | green << 8 | blue;
     }
 
@@ -207,7 +205,7 @@ class Color implements IInitializable
      * @param rgb    24-bit RGB.
      * @return       32-bit ARGB unsigned integer.
      */
-    public static function getARGBFromAlphaAndRGB(rgb : Int, alpha : Int = 0xff) : Int{
+    public static function getARGBFromAlphaAndRGB(rgb:Int, alpha:Int = 0xff):Int {
         return (alpha << 24) + rgb;
     }
 
@@ -225,7 +223,7 @@ class Color implements IInitializable
      * @param alpha  8-bit Alpha.
      * @return       32-bit RGBA unsigned integer.
      */
-    public static function getRGBA(red : Int, green : Int, blue : Int, alpha : Int) : Int{
+    public static function getRGBA(red:Int, green:Int, blue:Int, alpha:Int):Int {
         return red << 24 | green << 16 | blue << 8 | alpha;
     }
 
@@ -235,7 +233,7 @@ class Color implements IInitializable
      * @param color 32-bit ARGB unsigned int.
      * @return 32-bit RGBA unsigned int.
      */
-    public static function getRGBAFromARGB(color : Int) : Int{
+    public static function getRGBAFromARGB(color:Int):Int {
         return color << 8 | color >>> 24;
     }
 
@@ -245,7 +243,7 @@ class Color implements IInitializable
      * @param color 32-bit RGBA unsigned int.
      * @return 32-bit ARGB unsigned int.
      */
-    public static function getARGBFromRGBA(color : Int) : Int{
+    public static function getARGBFromRGBA(color:Int):Int {
         return color >>> 8 | color << 24;
     }
 
@@ -257,7 +255,7 @@ class Color implements IInitializable
      * @param blue   8-bit Blue.
      * @return       32-bit RGB unsigned integer.
      */
-    public static function getRGB(red : Int, green : Int, blue : Int) : Int{
+    public static function getRGB(red:Int, green:Int, blue:Int):Int {
         return red << 16 | green << 8 | blue;
     }
 
@@ -267,7 +265,7 @@ class Color implements IInitializable
      * @param color  32-bit ARGB color as unsigned integer.
      * @return       8-bit alpha.
      */
-    public static function getAlpha(color : Int) : Int{
+    public static function getAlpha(color:Int):Int {
         return color >>> 24;
     }
 
@@ -277,7 +275,7 @@ class Color implements IInitializable
      * @param color  24-bit RGB color as unsigned integer.
      * @return       8-bit red.
      */
-    public static function getRed(color : Int) : Int{
+    public static function getRed(color:Int):Int {
         return color >>> 16 & 0xFF;
     }
 
@@ -287,7 +285,7 @@ class Color implements IInitializable
      * @param color  24-bit RGB color as unsigned integer.
      * @return       8-bit green.
      */
-    public static function getGreen(color : Int) : Int{
+    public static function getGreen(color:Int):Int {
         return color >>> 8 & 0xFF;
     }
 
@@ -297,7 +295,7 @@ class Color implements IInitializable
      * @param color  24-bit RGB color as unsigned integer.
      * @return       8-bit blue.
      */
-    public static function getBlue(color : Int) : Int{
+    public static function getBlue(color:Int):Int {
         return color & 0xFF;
     }
 
