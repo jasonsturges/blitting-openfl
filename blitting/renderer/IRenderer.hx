9ff8ac4c55d4ddea42b43439b23f920729545fc5
Jason Sturges <jason@jasonsturges.com>
2017-03-30 23:06:30 -0500

    Renderer interface

--- blitting/renderer/IRenderer.hx
+++ blitting/renderer/IRenderer.hx
@@ -9,5 +9,17 @@
 */
 package blitting.renderer;
 
+import openfl.geom.Point;
+import openfl.geom.Rectangle;
+
 interface IRenderer {
+
+    public var bounds(get, set):Rectangle;
+    public function get_bounds():Rectangle;
+    public function set_bounds(value:Rectangle):Rectangle;
+
+    public var registration(get, set):Point;
+    public function get_registration():Point;
+    public function set_registration(value:Point):Point;
+
 }
