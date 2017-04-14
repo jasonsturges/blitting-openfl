856c82c562b7f7417b26000dfa213af264af5930
Jason Sturges <jason@jasonsturges.com>
2017-04-14 11:24:32 -0500

    Documentation

--- blitting/util/DateUtil.hx
+++ blitting/util/DateUtil.hx
@@ -12,42 +12,39 @@ package blitting.util;
 class DateUtil {
 
     /**
-        Whether the current time is Ante meridiem (AM) or
-        Post meridiem (PM), for 12-hour clock.
-    **/
-    public static function meridiem(d:Date):String {
-        return (d.getHours() > 11) ? "pm" : "am";
+     *  Whether the current time is Ante meridiem (AM) or
+     *  Post meridiem (PM), for 12-hour clock.
+     *  @param date
+     *  @return String
+     */
+    public static function meridiem(date:Date):String {
+        return (date.getHours() > 11) ? "pm" : "am";
     }
 
     /**
-        Whether daylight savings time is currently observed.
-        TODO: Not implemented, pending resolution of consistent
-              timezone offset.
-    **/
-    //public static function isDaylightSavings(d:Date):Bool {
-    //}
-
-    /**
-	    Whether the specified year is leap year.
-    **/
+     *  Whether the specified year is leap year.
+     *  @param year
+     *  @return Bool
+     */
     public static function isLeapYear(year:Int):Bool {
         return daysInMonth(year, 1) == 29;
     }
 
     /**
-        Number of days in the specified month.
-
-        @param year   Full year as Int (ex: 2000).
-        @param month  Month as Int, zero-based (ex: 0=January, 11=December).
-    **/
+     *  Number of days in the specified month.
+     *  @param year   Full year as Int (ex: 2000).
+     *  @param month  Month as Int, zero-based (ex: 0=January, 11=December). 
+     *  @return Int
+     */
     public static function daysInMonth(year:Int, month:Int):Int {
         return (new Date(year, ++month, 0, 0, 0, 0).getDate());
     }
 
     /**
-        Get today's date at start of day.
-        (zero-hour, at midnight).
-    **/
+     *  Get today's date at start of day - zero-hour, at midnight.
+     *  @param date   Optional date.
+     *  @return Date
+     */
     public static function today(date:Date = null):Date {
         if (date == null)
             date = Date.now();
