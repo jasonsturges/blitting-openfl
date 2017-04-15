fc234e270c392c60d3e0d5a3685316dac8b38f36
Jason Sturges <jason@jasonsturges.com>
2017-04-14 22:18:34 -0500

    Geometry class refinement

--- blitting/math/Geometry.hx
+++ blitting/math/Geometry.hx
@@ -24,8 +24,7 @@ class Geometry {
     }
 
     /**
-     * Distance between two points within the same
-     * coordinate system.
+     * Distance between two points within the same coordinate system.
      */
     public static function distance(x1:Float, y1:Float, x2:Float, y2:Float):Float {
         var dx:Float = x1 - x2;
@@ -50,35 +49,11 @@ class Geometry {
     }
 
     /**
-     * Returns the x, y coordinate point on a circle for a given angle in degrees.
-     */
-    public static function pointOnCircle(degrees:Float, radius:Float):Point {
-        var p:Point = new Point();
-
-        p.x = radius + (radius * Math.cos(Geometry.deg2rad(degrees)));
-        p.y = radius + (radius * Math.sin(Geometry.deg2rad(degrees)));
-
-        return p;
-    }
-
-    /**
-     * Cartesian point from circle center, angle, and radius.
-     */
-    public static function pointOnCircleFromPoint(center:Point, angle:Float, radius:Float):Point {
-        var p:Point = new Point();
-
-        p.x = center.x + Math.cos(angle * Math.PI / 180) * radius;
-        p.y = center.y + Math.sin(angle * Math.PI / 180) * radius;
-
-        return p;
-    }
-
-    /**
      * Returns the points on a bezier curve for a given time (0 - 1).
      */
     public static function pointOnCurve(p1x:Float, p1y:Float, cx:Float, cy:Float, p2x:Float, p2y:Float, t:Float):Point {
         return (new Point(p1x + t * (2 * (1 - t) * (cx - p1x) + t * (p2x - p1x)),
-        p1y + t * (2 * (1 - t) * (cy - p1y) + t * (p2y - p1y))));
+                          p1y + t * (2 * (1 - t) * (cy - p1y) + t * (p2y - p1y))));
     }
 
     /**
@@ -111,9 +86,9 @@ class Geometry {
      */
     public static function withinBounds(x:Float, y:Float, bounds:Rectangle):Bool {
         return ((x > bounds.x) &&
-        (x < bounds.x + bounds.width) &&
-        (y > bounds.y) &&
-        (y < bounds.y + bounds.height));
+                (x < bounds.x + bounds.width) &&
+                (y > bounds.y) &&
+                (y < bounds.y + bounds.height));
     }
 
 }
